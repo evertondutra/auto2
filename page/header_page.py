bde0dbedbe6aad651afc43da9e020af4b180ed72
Everton Dutra <evertondutra02@gmail.com>
2021-05-04 14:31:26 -0300

    criado uma class pra busca do imput e do botão submit

--- page/header_page.py
+++ page/header_page.py
@@ -1,9 +1,17 @@
 from browser import Browser
 
 
+class HeaderPageElements(object):
+    INPUT_BUSCA = 'id-search-field'
+
+
+class bot(object):
+    BTN_GO = 'submit'
+
+
 class HeaderPage(Browser):
     def preenche_imput_busca(self, texto):
-        self.driver.find_element_by_id('id-search-field').send_keys(texto)
+        self.driver.find_element_by_id(HeaderPageElements.INPUT_BUSCA).send_keys(texto)
 
     def click_btn_go(self):
-        self.driver.find_element_by_id('submit').click()
+        self.driver.find_element_by_id(bot.BTN_GO).click()
